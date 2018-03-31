9f99cc0033623e188de155816282caeb091bd274
Orcses30 <davidglong29@gmail.com>
2018-03-31 03:51:45 -0400

    turn off fog gem

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,11 +13,11 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #if Rails.env.production?
+    #storage :fog
+  #else
+    #storage :file
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
