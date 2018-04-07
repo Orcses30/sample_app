1fb50185f8575127446d8a08c532a8400728b128
Orcses30 <davidglong29@gmail.com>
2018-04-07 15:59:05 -0400

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -81,7 +81,36 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
-      @user.destroy 
+      @user.destroy
+    end
+  end
+
+  test "should follow and unfollow a user" do
+    david  = users(:david)
+    archer = users(:archer)
+    assert_not david.following?(archer)
+    david.follow(archer)
+    assert david.following?(archer)
+    assert archer.followers.include?(david)
+    david.unfollow(archer)
+    assert_not david.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    david  = users(:david)
+    archer = users(:archer)
+    lana   = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert david.feed.include?(post_following)
+    end
+    # Posts from self
+    david.microposts.each do |post_self|
+      assert david.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not david.feed.include?(post_unfollowed)
     end
   end
 end
